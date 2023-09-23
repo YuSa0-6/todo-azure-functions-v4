c509943ae1da22f2be98d726ad2187f5f9de3bb7
YuSa0-6 <fuku.sugar@icloud.com>
2023-09-23 19:00:18 +0900

    get Todos

---
+++ src/functions/getTodo.ts
@@ -0,0 +1,21 @@
+import { app, HttpRequest, HttpResponseInit, InvocationContext, input } from "@azure/functions";
+
+const sqlInput = input.sql({
+    commandText: 'select [Id], [title], [description], [status] from Todos',
+    commandType: 'Text',
+    connectionStringSetting: 'SqlConnectionString',
+})
+export async function getTodo(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
+    const toDoItems = context.extraInputs.get(sqlInput);
+    context.log('😇HTTP trigger adn SQL input binding function processed a request',{toDoItems});
+    return{
+        jsonBody: toDoItems,
+    }
+};
+
+app.http('getTodo', {
+    methods: ['GET'],
+    authLevel: 'anonymous',
+    extraInputs:[sqlInput],
+    handler: getTodo
+});
